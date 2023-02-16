8acac5b5e9db5b11a8eecee8f805c4a540e04eec
Allison D Binley <allisondbinley@gmail.com>
2023-02-16 11:37:37 -0500

    revisions

--- CitSci_bio_monitoring1/revisions.R
+++ CitSci_bio_monitoring1/revisions.R
@@ -96,7 +96,142 @@ our_dd_sps <- our_sps %in% dds$scientificName
 our_dd_sps_df <- data.frame(species = our_sps,
                             dd = our_dd_sps)
 
-#### geography ####
+
+#### r scopus ####
+
+install.packages("rscopus")
+library(rscopus)
+
+set_api_key("d6655768243098762322155806c1e7f0")
+
+# scopus search string: 
+# TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  "public participation"  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  "citizen researcher"  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (species)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer ) 
+
+# taxonomic 
+
+bird_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (bird* OR avian)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #732 cs bird papers
+
+bird <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (bird* OR avian)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #30897 bird papers
+
+
+invertebrate_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (invert*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") # 82 cs inv papers
+
+invertebrate <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (invert*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") # 13866
+
+
+mammal_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (mammal*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") # 361
+
+mammal <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (mammal*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") # 24705
+
+plant_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (plant*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #654
+
+plant <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (plant*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #124380
+
+
+amphibian_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (amphib*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #89
+
+amphibian <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (amphib*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #7794
+
+
+reptile_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (reptil*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #99
+
+reptile <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (reptil*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #4867
+
+
+fungi_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (fung*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #38
+
+fungi <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (fung*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #14990
+
+
+taxon_comp <- data.frame(taxa_clean = c("invertebrate","bird","mammal","amphibian","plant","reptile","fungi"),
+                         CS = c(82,732,361,89,654,99,38),
+                         all_lit = c(13866,30897,24705,7794,124380,4867,14990))
+
+taxon_comp$proportion_lit <- taxon_comp$all_lit/sum(taxon_comp$all_lit)
+
+res2 <- chisq.test(x = taxon_comp$CS, p = taxon_comp$proportion_lit, correct = F)
+
+res2$expected
+
+res2_summary <- data.frame("taxon" = taxon_comp$taxa_clean,
+                    "observed" = res2$observed,
+                    "expected" = res2$expected)
+
+#taxon_comp$difference <- taxon_comp$proportion - taxon_comp$proportion_lit
+
+taxon_comp_plot <- ggplot(res2_summary, aes(y=observed, x=expected, label = taxon))+
+  geom_point()+
+  #geom_text(hjust=0, vjust=-0.5)+
+  geom_label_repel(size = 7,aes(size = NULL), nudge_y = 0.7)+
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed")+
+  theme_classic(base_size = 24, base_family = "sans")+
+  labs(x="Expected Number of Articles", y="Observed Number of Articles")
+
+png("figures/fig_xsq2.png", height = 10, width = 10, units="in", res = 300)
+taxon_comp_plot
+dev.off()
+
+taxon_comp$CS_prop <- taxon_comp$CS/taxon_comp$all_lit
+
+
+# geography #
+
+North_America_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY ({North America})  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )") #260
+
+North_America <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY ({North America})  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)") #17934
+
+
+Europe_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Europ*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )")#678
+
+Europe <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Europ*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)")#44157
+
+
+Australia_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Australia* OR oceania)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )")#301
+
+Australia <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Australia* OR oceania)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)")#19853
+
+
+Africa_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Africa*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )")#502
+
+Africa <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY (Africa*)  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)")#25731
+
+
+
+South_America_cs <- scopus_search(query="TITLE-ABS-KEY ( {community based participatory research}  OR  {community-based participatory research}  OR  {public participation}  OR  hunter  OR  naturalist  OR  atlas*  OR  amateur  OR  {citizen researcher}  OR  {individual citizen scientist}  OR  collaborator  OR  {lay knowledge holder} )  AND  TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY ({South America})  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer )")#61
+
+South_America <- scopus_search(query="TITLE-ABS-KEY (ecolog*  OR  biodivers*  OR  conservation)  AND  TITLE-ABS-KEY ( monitor*  OR  manage*  OR  survey  OR  conserv* )  AND  TITLE-ABS-KEY ({South America})  AND NOT  TITLE-ABS-KEY (marine  OR  restoration  OR  astron*  OR  medic*  OR  cancer)")#6204
+
+
+geog_comp <- data.frame(continent = c("North America","Europe","Australia","Africa","South America"),
+                        CS = c(260,678,301,502,61),
+                        all_lit = c(17934,44157,19853,25731,6204))
+
+
+geog_comp$proportion_lit <- geog_comp$all_lit/sum(geog_comp$all_lit)
+
+res3 <- chisq.test(x = geog_comp$CS, p = geog_comp$proportion_lit, correct = F)
+
+res3$expected
+
+res3_summary <- data.frame("continent" = geog_comp$continent,
+                           "observed" = res3$observed,
+                           "expected" = res3$expected)
+
+#taxon_comp$difference <- taxon_comp$proportion - taxon_comp$proportion_lit
+
+geog_comp_plot <- ggplot(res3_summary, aes(y=observed, x=expected, label = continent))+
+  geom_point()+
+  #geom_text(hjust=0, vjust=-0.5)+
+  geom_label_repel(size = 7,aes(size = NULL), nudge_y = 0.7)+
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed")+
+  theme_classic(base_size = 24, base_family = "sans")+
+  labs(x="Expected Number of Articles", y="Observed Number of Articles")
+
+png("figures/fig_xsq3.png", height = 10, width = 10, units="in", res = 300)
+geog_comp_plot
+dev.off()
+
+geog_comp$CS_prop <- geog_comp$CS/geog_comp$all_lit
 
 # n_cs <- length(citsci_data1$ï..Program.Name)
 # 
